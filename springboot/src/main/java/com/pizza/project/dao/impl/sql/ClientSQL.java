c802eaf2759fcd2f2edffd935d6a113372728591
AndriiChemer <ichemer@icloud.com>
2017-10-28 17:22:13 +0300

    add - ClientSQL

---
+++ springboot/src/main/java/com/pizza/project/dao/impl/sql/ClientSQL.java
@@ -0,0 +1,13 @@
+package com.pizza.project.dao.impl.sql;
+
+public final class ClientSQL {
+    public static final String PARAM_TABLE = "client";
+    public static final String PARAM_ID = "id";
+    public static final String PARAM_NAME = "name";
+    public static final String PARAM_SURNAME = "surname";
+    public static final String PARAM_EMAIL = "email";
+    public static final String PARAM_PHONE = "phone";
+    public static final String PARAM_PASSWORD = "password";
+    public static final String PARAM_ID_ROLE = "id_role";
+
+}
