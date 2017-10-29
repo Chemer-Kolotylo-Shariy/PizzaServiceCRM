98067c54ed43a230be90b6163f60b61fa0e371e6
AndriiChemer <ichemer@icloud.com>
2017-10-29 20:25:43 +0200

    add - ClientAddressImpl

---
+++ springboot/src/main/java/com/pizza/project/dao/impl/ClientAddressImpl.java
@@ -0,0 +1,85 @@
+package com.pizza.project.dao.impl;
+
+import com.pizza.project.dao.AddressDao;
+import com.pizza.project.dao.ClientAddressDao;
+import com.pizza.project.dao.ClientDao;
+import com.pizza.project.dao.impl.sql.ClientAddressSQL;
+import com.pizza.project.model.ClientAddress;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ClientAddressImpl implements ClientAddressDao {
+    private SimpleJdbcInsert simpleJdbcInsert;
+    private NamedParameterJdbcTemplate jdbcTemplate;
+    private ClientAddressExtractor clientAddressExtractor;
+
+    public ClientAddressImpl(DataSource dataSource, ClientDao clientDao, AddressDao addressDao) {
+        simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName(ClientAddressSQL.PARAM_TABLE);
+        jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        clientAddressExtractor = new ClientAddressExtractor(clientDao, addressDao);
+    }
+
+    @Override
+    public List<ClientAddress> getAll() {
+        return jdbcTemplate.query(ClientAddressSQL.QUERY_GET_ALL, clientAddressExtractor);
+    }
+
+    @Override
+    public ClientAddress get(Long id) {
+        return null;
+    }
+
+    @Override
+    public Long create(ClientAddress object) {
+        return null;
+    }
+
+    @Override
+    public Long update(ClientAddress object) {
+        return null;
+    }
+
+    @Override
+    public Long remove(Long id) {
+        return null;
+    }
+
+    private class ClientAddressExtractor implements ResultSetExtractor<List<ClientAddress>> {
+        private ClientDao clientDao;
+        private AddressDao addressDao;
+
+        @Autowired
+        public ClientAddressExtractor(ClientDao clientDao, AddressDao addressDao) {
+            this.clientDao = clientDao;
+            this.addressDao = addressDao;
+        }
+
+        @Override
+        public List<ClientAddress> extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+            List<ClientAddress> clientAddresses = new ArrayList<>();
+
+            while (resultSet.next()){
+                ClientAddress clientAddress = new ClientAddress();
+
+                clientAddress.setClient(clientDao.get(resultSet.getLong(ClientAddressSQL.PARAM_ID_CLIENT)));
+                clientAddress.setAddress(addressDao.get(resultSet.getLong(ClientAddressSQL.PARAM_ID_ADDRESS)));
+
+                clientAddresses.add(clientAddress);
+            }
+            return clientAddresses;
+        }
+    }
+}
