70b0faa50149b192c9dc2b8affcb876451d6749c
AndriiChemer <ichemer@icloud.com>
2017-11-01 17:18:27 +0200

    add - CategorySQL

---
+++ springboot/src/main/java/com/pizza/project/dao/impl/sql/CategorySQL.java
@@ -0,0 +1,10 @@
+package com.pizza.project.dao.impl.sql;
+
+public class CategorySQL {
+    public static final String PARAM_TABLE = "category";
+    public static final String PARAM_ID = "id";
+    public static final String PARAM_CATEGORY = "category";
+
+    public static final String QUERY_GET_ALL_CATEGORY = "SELECT * FROM category;";
+
+}
