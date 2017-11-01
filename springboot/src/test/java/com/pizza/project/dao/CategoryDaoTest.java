54940937601e38ad57375ba1a1db60734e3b2488
AndriiChemer <ichemer@icloud.com>
2017-11-01 17:22:35 +0200

    add - CategoryDaoTest

---
+++ springboot/src/test/java/com/pizza/project/dao/CategoryDaoTest.java
@@ -0,0 +1,34 @@
+package com.pizza.project.dao;
+
+import com.pizza.project.model.Category;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryDaoTest {
+    @Autowired
+    CategoryDao categoryDao;
+
+    @Test
+    public void create(){
+        Integer id = categoryDao.createCategory(new Category("NAPOJ"));
+        Assert.assertNotNull("Category haven`t been created! ", id);
+        System.out.println("Category was created " + 1);
+    }
+
+    @Test
+    public void getAll(){
+        List<Category> categoryList = categoryDao.getAllCategories();
+        Assert.assertNotNull("Category not found!", categoryList);
+        for (Category categ : categoryList) {
+            System.out.println("" + categ.getId() + ". " + categ.getCategory());
+        }
+    }
+}
