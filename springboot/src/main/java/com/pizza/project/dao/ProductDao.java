cd11fc09edb5e1fc5e6be3ecf621b9711074a49f
AndriiChemer <ichemer@icloud.com>
2017-11-01 19:56:24 +0200

    update - ProductDao

--- springboot/src/main/java/com/pizza/project/dao/ProductDao.java
+++ springboot/src/main/java/com/pizza/project/dao/ProductDao.java
@@ -2,5 +2,9 @@ package com.pizza.project.dao;
 
 import com.pizza.project.model.Product;
 
+import java.util.List;
+
 public interface ProductDao extends CrudDao<Product> {
+    List<Product> getProductsByCategory(String category);
+
 }
