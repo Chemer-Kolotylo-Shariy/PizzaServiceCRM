6f944f475ebeae48e52964d619d86a204ab0c62a
AndriiChemer <ichemer@icloud.com>
2017-11-01 17:19:25 +0200

    add - CategoryDao

---
+++ springboot/src/main/java/com/pizza/project/dao/CategoryDao.java
@@ -0,0 +1,10 @@
+package com.pizza.project.dao;
+
+import com.pizza.project.model.Category;
+
+import java.util.List;
+
+public interface CategoryDao extends CrudDao<Category> {
+    List<Category> getAllCategories();
+    Integer createCategory(Category object);
+}
