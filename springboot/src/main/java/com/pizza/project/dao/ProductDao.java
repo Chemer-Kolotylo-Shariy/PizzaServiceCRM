f1028606244a59f1c55bb60c33c877e80017a6be
AndriiChemer <ichemer@icloud.com>
2017-11-07 14:09:39 +0200

    update - ProductDao

--- springboot/src/main/java/com/pizza/project/dao/ProductDao.java
+++ springboot/src/main/java/com/pizza/project/dao/ProductDao.java
@@ -3,8 +3,12 @@ package com.pizza.project.dao;
 import com.pizza.project.model.Product;
 
 import java.util.List;
-
+/**
+ * Create by Andrii Chemer
+ * */
 public interface ProductDao extends CrudDao<Product> {
     List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByCategoryAndAmountNotNull(String category);
+
 
 }
