e4dcc64a000dbe568316acf39f68ba3cdc87e5c3
AndriiChemer <ichemer@icloud.com>
2017-11-21 21:50:54 +0200

    add - model OrderProduct

---
+++ springboot/src/main/java/com/pizza/project/model/OrderProduct.java
@@ -0,0 +1,40 @@
+package com.pizza.project.model;
+
+public class OrderProduct {
+
+    private Order order;
+    private Product product;
+    private Integer countProduct;
+
+    public OrderProduct() {
+    }
+
+    public OrderProduct(Order order, Product product) {
+        this.order = order;
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getCountProduct() {
+        return countProduct;
+    }
+
+    public void setCountProduct(Integer countProduct) {
+        this.countProduct = countProduct;
+    }
+}
